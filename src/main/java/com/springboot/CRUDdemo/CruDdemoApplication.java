08dd09f137c0409a70b49876839b8027187891ac
Chadiii <chadilaoulaou@gmail.com>
2020-06-02 23:15:28 +0000

    starting CRUD Application

---
+++ src/main/java/com/springboot/CRUDdemo/CruDdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.CRUDdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDdemoApplication.class, args);
+	}
+
+}
