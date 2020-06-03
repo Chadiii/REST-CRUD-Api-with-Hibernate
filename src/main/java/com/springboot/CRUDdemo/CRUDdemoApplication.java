616f28c74859467d2edbedcf6906859c81ff25ab
Chadiii <chadilaoulaou@gmail.com>
2020-06-03 01:14:19 +0000

    Adding Entity Student

--- src/main/java/com/springboot/CRUDdemo/CRUDdemoApplication.java
+++ src/main/java/com/springboot/CRUDdemo/CRUDdemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruDdemoApplication {
+public class CRUDdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruDdemoApplication.class, args);
+		SpringApplication.run(CRUDdemoApplication.class, args);
 	}
 
 }
